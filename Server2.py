781cb6643d94e4c5c2b5287abe9a7c5d3bde6433
Matheus Souza dos Santos <msdsantos96@gmail.com>
2019-11-20 02:05:20 -0300

    att

--- Server2.py
+++ Server2.py
@@ -6,5 +6,7 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
+    print("Disponivel na Porta ", PORT)
     httpd.serve_forever()
+    #pagina de apoio
+    # https://docs.python.org/3/library/http.server.html#module-http.server
