77346403c664b13d754e77a01f551e79af56d27c
Matheus Souza dos Santos <msdsantos96@gmail.com>
2019-11-20 01:39:12 -0300

    Atualizaçao

--- Client.py
+++ Client.py
@@ -1,12 +1,13 @@
-
-
 import socket 
 ip = input('digite o ip de conexao: ') 
 port = 7000 
 addr = ((ip,port)) 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 client_socket.connect(addr) 
-mensagem = input("digite uma mensagem para enviar ao servidor") 
-client_socket.send(mensagem) 
+mensagem = input("digite uma mensagem para enviar ao servidor: ") 
+client_socket.send(mensagem.encode('utf-8')) 
 print ('mensagem enviada') 
+#con= client_socket.accept() 
+#resposta = con.recv(1024) 
+#print (resposta.decode('utf-8'))
 client_socket.close()
--- Server.py
+++ Server.py
@@ -1,5 +1,6 @@
 
 import socket 
+import webbrowser 
 
 
 host = '' 
@@ -9,15 +10,15 @@ addr = (host, port)
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 serv_socket.bind(addr) 
-serv_socket.listen(10) 
-print ('aguardando conexao' )
-con, cliente = serv_socket.accept() 
-print ('conectado') 
-print ("aguardando mensagem" )
-recebe = con.recv(1024) 
-print ("mensagem recebida: "+ recebe) 
+
+while 1: 
+    serv_socket.listen(10) 
+    print ('aguardando conexao' )
+    con, cliente = serv_socket.accept() 
+    print ('conectado com: ' )
+    print (cliente)
+    print ("aguardando mensagem" )
+    recebe = con.recv(1024) 
+    print('recebido: '+recebe.decode('utf-8'))
+    #webbrowser.open_new(recebe)
 serv_socket.close() 
-
-
-
-
